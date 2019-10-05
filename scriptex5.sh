f2a96b2da6d73a8afa41d8d8e9637b8fb77ede44
Nic Chong <nchong@nd.edu>
2019-10-05 19:10:11 -0400

    Uploading sorting by wage (top/bottom earner; females in top 10)

--- scriptex5.sh
+++ scriptex5.sh
@@ -1,1 +1,11 @@
 sed -E 's/,/ /g' wages.csv | cut -d ' ' -f 1,2 | sort -k1,1 -k2n,2 > sortedwage.txt
+
+echo "Number of top female earners:"
+sort -n -k 4 wages.csv | tail -n 10 | grep -E "female" | wc -l > Topearners.csv
+cat Topearners.csv
+echo "Top earner:"
+sort -n -k 4 wages.csv | cut -d ',' -f 1,2,4 | tail -n 1  
+echo "Bottom earner:"
+sort -n -k 4 wages.csv | cut -d ',' -f 1,2,4 | head -n 1
+
+
